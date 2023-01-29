7af23b5592c3193ba495074df9d669e385046d0b
leesr1972 <sergey.r.lee@gmail.com>
2023-01-29 13:34:19 +0300

    Initial commit.

---
+++ src/main/java/sky/pro/java/skyprotz/SkyProTzApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.java.skyprotz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProTzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProTzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/java/skyprotz/SkyProTzApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.java.skyprotz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyProTzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
